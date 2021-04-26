1c15015c645ec1639ca87ec037e46adfa7d584b9
Barbara Bogaczyk <bbogaczyk@gmail.com>
2021-04-26 20:16:34 +0800

    files changed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+        
+        //reduce the opacity when the button get pressed to half
+        UIView.animate(withDuration: 0.3) {
+                sender.alpha = 0.5
+            }
+        
+        // back the pressed button to full opacity
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 ) {
+            UIView.animate(withDuration: 0.3) {
+                sender.alpha = 1.0
+                }
+            }
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+        
+                
+    }
 }
 
